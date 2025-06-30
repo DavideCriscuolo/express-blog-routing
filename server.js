5d2aa60ef61ef2afbc609732d11c69373aa7b19f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-30 14:41:46 +0200

    feat: implement CRUD routes for posts in the blog application

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const PostsRouter = require("./routes/posts");
 
 app.listen(port, () => {
   console.log(`Il server è in ascolto`);
 });
+app.use("/posts", PostsRouter);
